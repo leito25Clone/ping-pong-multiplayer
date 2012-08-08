825f8b0ffc5c1c3bd80d2b4a64139eab1494f30e
Ilya Pirozhenko <ilya.pir@gmail.com>
2012-08-08 22:37:13 +0400

    Initial commit

---
+++ game.js
@@ -0,0 +1,101 @@
+window.onload = function () {
+
+	console.log("game.js succesfully loaded");
+	var canvas = document.getElementById('gameCanvas'),
+		context = canvas.getContext("2d"),
+		redBall = new Ball(),
+		greenBall = new Ball(20, "green"),
+		paddle = new Paddle(),
+		centerX = canvas.width / 2,
+		centerY = canvas.height / 2,
+		angle = 0,
+		radiusX = 10,
+		radiusY = 10,
+		mouse = utils.captureMouse(canvas);
+
+	redBall.x = centerX + 20;
+	redBall.y = centerY - 123;
+	
+	greenBall.x = centerX - 100;
+	greenBall.y = centerY ;
+
+	paddle.x = centerX;
+	paddle.y = centerY;
+
+	var onKeyPressed = function (event) {
+		switch (event.keyCode) {
+			case 38:  //TODO: change to consts
+				console.log("Key Up Pressed!");
+				paddle.moveUp();
+				break;
+			case 40:
+				console.log("Key Down Pressed!");
+				paddle.moveDown();
+				break;
+			default:
+				console.log(event.type);
+				break;
+		}
+	};
+
+	//window.addEventListener('keyup', onKeyPressed, false);
+	window.addEventListener('keydown', onKeyPressed, false);
+	canvas.addEventListener('mousemove', function () {
+		paddle.move(mouse.y, canvas);
+		console.log("x: " + mouse.x + ", y: " + mouse.y);
+	}, false);
+
+	if (!window.requestAnimationFrame) {
+		window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {
+			return window.setTimeout(callback, 1000/60);
+		});
+	}
+
+	(function drawFrame () {
+		window.requestAnimationFrame(drawFrame, canvas);
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		if (utils.intersects(greenBall.getBounds(), redBall.getBounds()) === true) {
+			var tempX = greenBall.speedX,
+				tempY = greenBall.speedY;
+			greenBall.speedX = redBall.speedX;
+			greenBall.speedY = redBall.speedY;
+			redBall.speedX = tempX;
+			redBall.speedY = tempY;
+			
+			console.log("Intersect: Red Ball & Green Ball");
+		}
+
+		if (utils.intersects(greenBall.getBounds(), paddle.getBounds()) === true) {
+			greenBall.speedX *= -1;
+			greenBall.speedY *= -1;
+			console.log("Intersect: Green Ball & Paddle");
+		}
+
+		if (utils.intersects(redBall.getBounds(), paddle.getBounds()) === true) {
+			redBall.speedX *= -1;
+			redBall.speedY *= -1;
+			console.log("Intersect: Red Ball & Paddle");
+		}
+/*
+		greenBall.testCollision(redBall.getBounds());
+		redBall.testCollision(greenBall.getBounds());
+*/		
+		greenBall.testBorderCollision(canvas);
+		greenBall.move();
+		greenBall.draw(context);
+
+
+		redBall.testBorderCollision(canvas);
+		redBall.move();
+		redBall.draw(context);
+
+		paddle.draw(context);
+		
+	}());
+
+};
---
+++ utils.js
@@ -0,0 +1,35 @@
+utils = {};
+
+utils.intersects = function (rectA, rectB) {
+	return !(rectA.x + rectA.width < rectB.x ||
+			rectB.x + rectB.width < rectA.x ||
+			rectA.y + rectA.height < rectB.y ||
+			rectB.y + rectB.height < rectA.y);
+};
+
+utils.captureMouse = function (element) {
+	var mouse = {x: 0, y: 0};
+	element.addEventListener('mousemove', function (event) {
+		var x, y;
+		if (event.pageX || event.pageY) {
+			x = event.pageX;
+			y = event.pageY;
+		} else {
+			x = event.clientX + document.body.scrollLeft +
+			document.documentElement.scrollLeft;
+			y = event.clientY + document.body.scrollTop +
+			document.documentElement.scrollTop;
+		}
+		x -= element.offsetLeft;
+		y -= element.offsetTop;
+		mouse.x = x;
+		mouse.y = y;
+		}, false);
+
+	return mouse;
+};
+
+// metrics of Euklid space
+utils.getDistance = function (p1, p2) {
+	return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+};
